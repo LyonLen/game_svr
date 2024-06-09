f519a1d1cee406d94e7835dc5785cd03647b3113
lyonlin <397835809@qq.com>
2024-06-09 16:45:24 +0800

    增加一些注释。

--- src/effect_val.py
+++ src/effect_val.py
@@ -4,6 +4,22 @@ import math
 
 @dataclasses.dataclass
 class EffectVal(object):
+    __doc__ = """
+EffectVal设计思想：
+1 满足多模块能够对着同一个游戏数值效果累积数值，并具有上限。这种场景会出现的问题如下：
+    比如：a模块+2000，b模块+1000，limit为1500；对外表现，则是1500；这里是不识别变更值的，固然算变更值性能更好
+        但是，为了代码的简单，这里每次都重复计算，_value会存储一个溢出值。b模块移除，则表现在外部还会为1500，因为a模块足以填充满limit。
+2 _module_rates和_global_rate的设计，满足了游戏策划的扩展性设计：
+    对某个模块的所有效果加成；
+    对此效果，无视模块，统统加成。
+
+策划可设计的效果举例：
+    1 攻击力数值加成10；
+    2 攻击力百分比加成10%；
+    3 攻击力数值和百分比混合加成的结果，受limit限制，子模块无需关注是否溢出，算出来多少往里根据数值/百分比累积即可；
+    4 “基于某个模块”的攻击力加成百分比。
+"""
+
     _value: float = 0
     _limit: int = 0
     _module_values: dict = dataclasses.field(default_factory=dict)
